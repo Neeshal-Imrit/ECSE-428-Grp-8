a5a649dfc7d02230090a95f8fd27bbba3397ba9e
Neeshal <neeshalimrit@outlook.com>
2025-02-06 15:25:06 -0500

    Set UP model, database, backend

---
+++ Postr-Backend/src/main/java/ca/mcgill/ecse428/postr/model/Poster.java
@@ -0,0 +1,117 @@
+package ca.mcgill.ecse428.postr.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "poster")
+public class Poster
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //Poster Attributes
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String url;
+
+  //Poster Associations
+  @ManyToOne
+  private User user;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Poster(Long aId, String aUrl, User aUser)
+  {
+    id = aId;
+    url = aUrl;
+    boolean didAddUser = setUser(aUser);
+    if (!didAddUser)
+    {
+      throw new RuntimeException("Unable to create poster due to user. See https://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+    }
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setId(Long aId)
+  {
+    boolean wasSet = false;
+    id = aId;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setUrl(String aUrl)
+  {
+    boolean wasSet = false;
+    url = aUrl;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public Long getId()
+  {
+    return id;
+  }
+
+  public String getUrl()
+  {
+    return url;
+  }
+  /* Code from template association_GetOne */
+  public User getUser()
+  {
+    return user;
+  }
+  /* Code from template association_SetOneToMany */
+  public boolean setUser(User aUser)
+  {
+    boolean wasSet = false;
+    if (aUser == null)
+    {
+      return wasSet;
+    }
+
+    User existingUser = user;
+    user = aUser;
+    if (existingUser != null && !existingUser.equals(aUser))
+    {
+      existingUser.removePoster(this);
+    }
+    user.addPoster(this);
+    wasSet = true;
+    return wasSet;
+  }
+
+  public void delete()
+  {
+    User placeholderUser = user;
+    this.user = null;
+    if(placeholderUser != null)
+    {
+      placeholderUser.removePoster(this);
+    }
+  }
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "url" + ":" + getUrl()+ "]" + System.getProperties().getProperty("line.separator") +
+            "  " + "id" + "=" + (getId() != null ? !getId().equals(this)  ? getId().toString().replaceAll("  ","    ") : "this" : "null") + System.getProperties().getProperty("line.separator") +
+            "  " + "user = "+(getUser()!=null?Integer.toHexString(System.identityHashCode(getUser())):"null");
+  }
+}
---
+++ Postr-Backend/src/main/java/ca/mcgill/ecse428/postr/model/User.java
@@ -0,0 +1,197 @@
+package ca.mcgill.ecse428.postr.model;
+
+import java.util.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //User Attributes
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String email;
+  private String password;
+
+  //User Associations
+  @OneToMany(mappedBy = "user")
+  private List<Poster> posters;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public User(String aEmail, String aPassword)
+  {
+    email = aEmail;
+    password = aPassword;
+    posters = new ArrayList<Poster>();
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setEmail(String aEmail)
+  {
+    boolean wasSet = false;
+    email = aEmail;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public Long getId()
+  {
+    return id;
+  }
+
+
+  public boolean setPassword(String aPassword)
+  {
+    boolean wasSet = false;
+    password = aPassword;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getEmail()
+  {
+    return email;
+  }
+
+  public String getPassword()
+  {
+    return password;
+  }
+  /* Code from template association_GetMany */
+  public Poster getPoster(int index)
+  {
+    Poster aPoster = posters.get(index);
+    return aPoster;
+  }
+
+  public List<Poster> getPosters()
+  {
+    List<Poster> newPosters = Collections.unmodifiableList(posters);
+    return newPosters;
+  }
+
+  public int numberOfPosters()
+  {
+    int number = posters.size();
+    return number;
+  }
+
+  public boolean hasPosters()
+  {
+    boolean has = posters.size() > 0;
+    return has;
+  }
+
+  public int indexOfPoster(Poster aPoster)
+  {
+    int index = posters.indexOf(aPoster);
+    return index;
+  }
+  /* Code from template association_MinimumNumberOfMethod */
+  public static int minimumNumberOfPosters()
+  {
+    return 0;
+  }
+  /* Code from template association_AddManyToOne */
+  public Poster addPoster(Long aId, String aUrl)
+  {
+    return new Poster(aId, aUrl, this);
+  }
+
+  public boolean addPoster(Poster aPoster)
+  {
+    boolean wasAdded = false;
+    if (posters.contains(aPoster)) { return false; }
+    User existingUser = aPoster.getUser();
+    boolean isNewUser = existingUser != null && !this.equals(existingUser);
+    if (isNewUser)
+    {
+      aPoster.setUser(this);
+    }
+    else
+    {
+      posters.add(aPoster);
+    }
+    wasAdded = true;
+    return wasAdded;
+  }
+
+  public boolean removePoster(Poster aPoster)
+  {
+    boolean wasRemoved = false;
+    //Unable to remove aPoster, as it must always have a user
+    if (!this.equals(aPoster.getUser()))
+    {
+      posters.remove(aPoster);
+      wasRemoved = true;
+    }
+    return wasRemoved;
+  }
+  /* Code from template association_AddIndexControlFunctions */
+  public boolean addPosterAt(Poster aPoster, int index)
+  {  
+    boolean wasAdded = false;
+    if(addPoster(aPoster))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfPosters()) { index = numberOfPosters() - 1; }
+      posters.remove(aPoster);
+      posters.add(index, aPoster);
+      wasAdded = true;
+    }
+    return wasAdded;
+  }
+
+  public boolean addOrMovePosterAt(Poster aPoster, int index)
+  {
+    boolean wasAdded = false;
+    if(posters.contains(aPoster))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfPosters()) { index = numberOfPosters() - 1; }
+      posters.remove(aPoster);
+      posters.add(index, aPoster);
+      wasAdded = true;
+    } 
+    else 
+    {
+      wasAdded = addPosterAt(aPoster, index);
+    }
+    return wasAdded;
+  }
+
+  public void delete()
+  {
+    for(int i=posters.size(); i > 0; i--)
+    {
+      Poster aPoster = posters.get(i - 1);
+      aPoster.delete();
+    }
+  }
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "password" + ":" + getPassword()+ "]" + System.getProperties().getProperty("line.separator") +
+            "  " + "email" + "=" + (getEmail() != null ? !getEmail().equals(this)  ? getEmail().toString().replaceAll("  ","    ") : "this" : "null");
+  }
+}
