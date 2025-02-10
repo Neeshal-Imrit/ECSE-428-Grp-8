33e68f80f9561cc71a023bcf5f3dc837a2f7c2be
Jason-0218 <jasonshao0218@gmail.com>
2025-02-09 20:08:03 -0500

    finish REST web service and API endpoints

--- Postr-Backend/src/main/java/ca/mcgill/ecse428/postr/dto/PosterDTO.java
+++ Postr-Backend/src/main/java/ca/mcgill/ecse428/postr/dto/PosterDTO.java
@@ -1,5 +1,57 @@
 package ca.mcgill.ecse428.postr.dto;
 
+import ca.mcgill.ecse428.postr.model.Poster;
+
 public class PosterDTO {
+
+    private Long id;
+    private String url;
+    private Long userId;
+
+
+    public PosterDTO() {
+    }
     
+    public PosterDTO(Long id, String url, Long userId) {
+        this.id = id;
+        this.url = url;
+        this.userId = userId;
+    }
+
+    // Optional: Constructor to easily convert a Poster entity to a DTO.
+    // Note: This requires that your User entity has a getId() method.
+    public PosterDTO(Poster poster) {
+        if (poster != null) {
+            this.id = poster.getId();
+            this.url = poster.getUrl();
+            // Check that the associated user is not null before accessing its id
+            this.userId = poster.getUser() != null ? poster.getUser().getId() : null;
+        }
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
--- Postr-Backend/src/main/java/ca/mcgill/ecse428/postr/service/PosterService.java
+++ Postr-Backend/src/main/java/ca/mcgill/ecse428/postr/service/PosterService.java
@@ -1,5 +1,36 @@
 package ca.mcgill.ecse428.postr.service;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ca.mcgill.ecse428.postr.dao.PosterRepository;
+import ca.mcgill.ecse428.postr.model.Poster;
+import jakarta.transaction.Transactional;
+
+@Service
 public class PosterService {
+    @Autowired
+    private PosterRepository posterRepository;
     
+    //find all posters
+    public List<Poster> findAll() {
+        return posterRepository.findAll();
+    }
+
+    //find poster by id
+    public Poster findPosterById(Long id) {
+        return posterRepository.findPosterById(id);
+    }
+
+    //find poster by user id
+    public List<Poster> findByUserId(Long userId) {
+        return posterRepository.findByUserId(userId);
+    }
+
+    //save poster
+    @Transactional
+    public Poster savePoster(Poster poster) {
+        return posterRepository.save(poster);
+    }
+
 }
