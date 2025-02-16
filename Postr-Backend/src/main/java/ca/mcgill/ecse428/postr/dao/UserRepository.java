367505d610823e5a9a3732d66937c62c8f419e55
Mathieu <mathieu.allaire2@mail.mcgill.ca>
2025-02-16 15:16:33 -0500

    Finished, Poster service upload, missing service tests

--- Postr-Backend/src/main/java/ca/mcgill/ecse428/postr/dao/UserRepository.java
+++ Postr-Backend/src/main/java/ca/mcgill/ecse428/postr/dao/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findUserById(Long id);
+
+    User findUserByEmail(String email);
 }
--- Postr-Backend/src/main/java/ca/mcgill/ecse428/postr/service/PosterService.java
+++ Postr-Backend/src/main/java/ca/mcgill/ecse428/postr/service/PosterService.java
@@ -7,13 +7,17 @@ import org.springframework.http.HttpStatus;
 
 import ca.mcgill.ecse428.postr.exception.PostrException;
 import ca.mcgill.ecse428.postr.dao.PosterRepository;
+import ca.mcgill.ecse428.postr.dao.UserRepository;
 import ca.mcgill.ecse428.postr.model.Poster;
+import ca.mcgill.ecse428.postr.model.User;
 import jakarta.transaction.Transactional;
 
 @Service
 public class PosterService {
     @Autowired
     private PosterRepository posterRepository;
+    @Autowired
+    private UserRepository userRepository;
     
     //find all posters
     public List<Poster> findAll() {
@@ -30,6 +34,13 @@ public class PosterService {
         return posterRepository.findByUserId(userId);
     }
 
+    public List<Poster> findByUserEmail(String userEmail) {
+        return posterRepository.findByUserEmail(userEmail);
+    }
+    public Poster findByTitle(String title) {
+        return posterRepository.findPosterByTitle(title);
+    }
+
     //save poster
     @Transactional
     public Poster savePoster(Poster poster) {
@@ -38,9 +49,44 @@ public class PosterService {
 
     @Transactional
     public Poster uploadPoster(String userEmail, byte[] imageData, float price, String description, String title ){
-        
-        //throw new PostrException(HttpStatus.BAD_REQUEST, "The poster is empty");
+        if(userEmail == null){
+            throw new PostrException(HttpStatus.BAD_REQUEST, "The poster uploader has no email");
+        }
+        User user = userRepository.findUserByEmail(userEmail);
+        if(user == null){
+            throw new PostrException(HttpStatus.BAD_REQUEST, "The user's email cannot be found");
+        }
 
-        
+        if(imageData == null){
+            throw new PostrException(HttpStatus.BAD_REQUEST, "There is no image for the poster");
+        }
+
+        if(price < 0){
+            throw new PostrException(HttpStatus.BAD_REQUEST, "The poster's price cannot be negative");
+        }
+
+        if(description == null){
+            throw new PostrException(HttpStatus.BAD_REQUEST, "The description cannot be empty");
+        }
+
+        if(title == null){
+            throw new PostrException(HttpStatus.BAD_REQUEST, "The poster must have a title");
+        }
+        Poster aPoster = posterRepository.findPosterByTitle(title);
+
+        if(aPoster != null){
+            throw new PostrException(HttpStatus.BAD_REQUEST, "A poster with the same title already exists");
+
+        }
+
+        Poster poster = new Poster();
+        poster.setDescription(description);
+        poster.setImageData(imageData);
+        poster.setPrice(price);
+        poster.setTitle(title);
+        poster.setUser(user);
+
+        posterRepository.save(poster);
+        return poster;
     }
 }
